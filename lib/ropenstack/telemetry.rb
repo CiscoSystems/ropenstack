4b555ab79da36445f7b017cb0cd5421257236da5
Sam Betts <sam@code-smash.net>
2014-06-16 10:26:29 +0100

    Fill out telemetry API

--- lib/ropenstack/telemetry.rb
+++ lib/ropenstack/telemetry.rb
@@ -10,33 +10,58 @@ module Ropenstack
 =end
   class Telemetry < OpenstackService
     def alarms(id = nil)
+      if id.nil?
+        get_request(address("alarms"), @token)
+      else
+        get_request(address("alarms/"+id), @token)
+      end
     end
 
     def alarm_create
+      data = {}
+      post_request(address("alarms"), data, @token)
     end
 
     def alarm_update
+      data = {}
+      put_request(address("alarms"), data, @token)
     end
 
     def alarm_delete(id)
+      delete_request(address("alarms/"+id), @token)
     end
 
     def alarm_state(id)
+      get_request(address("alarms/"+id+"/state"), @token)
     end
 
     def alarm_history(id)
+      get_request(address("alarms/"+id+"/history"), @token)
     end
 
     def meters(name = nil)
+      if name.nil?
+        get_request(address("meters"), @token)
+      else
+        get_request(address("meters/"+name), @token)
+      end
     end
 
-    def samples 
+    def send_samples(name)
+      data = {}
+      post_request(address("meters/"+name), data, @token)
     end
 
     def meter_statistics(meter)
+      get_request(address("meters/"+name+"/statistics"), @token)
     end
 
     def resources(id = nil)
+      if id.nil?
+        get_request(address("resources"), @token)
+      else
+        get_request(address("resources/"+id), @token)
+      end
     end
   end
 end
